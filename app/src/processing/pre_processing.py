ab118230c02d116d68066a4d5b620b02e70f4ccf
frejalomborgrasmussen <frejalrasmussen@gmail.com>
2024-05-11 14:58:50 +0200

    preprocessing added again

--- app/src/processing/pre_processing.py
+++ app/src/processing/pre_processing.py
@@ -1,93 +1,44 @@
-import chess.pgn
-import pandas as pd
-import numpy as np
-import tqdm
-import ftfy
+def shortenGameMeta(games, player_name='Apendra'):
+    games_shorter = []
 
-pd.options.display.max_columns = 999
+    for game in games:
+        if game.get('variant') == 'standard':
+            moves_list = game.get('moves', '').split()
+            midgame_index = game.get('division', {}).get('middle', 0)
+            # Ensure we slice up to midgame + 8 moves
+            moves_up_to_midgame_plus_8 = ' '.join(moves_list[:midgame_index + 8])
 
-file_name = "apendra_games"
-player_name = "Apendra"
-NUM_GAMES = 1800
+            # Determine the game outcome for the player
+            if game.get('winner'):
+                if game['players']['white']['user']['name'] == player_name and game['winner'] == 'white':
+                    win_status = 'Won'
+                elif game['players']['black']['user']['name'] == player_name and game['winner'] == 'black':
+                    win_status = 'Won'
+                elif game['winner'] != 'draw' and (game['players']['white']['user']['name'] == player_name or game['players']['black']['user']['name'] == player_name):
+                    win_status = 'Lost'
+                else:
+                    win_status = 'Draw'
+            else:
+                win_status = 'Draw'  # Handle cases where there might not be a clear winner recorded
 
-def read_pgn_create_dataframe(file_name, num_games, player_name):
-    rows = []
-    with open(f'../../testData/lichess_db_standard_rated_2013-04/{file_name}.pgn') as pgn:
-        for _ in tqdm.tqdm(range(num_games)):
-            row = {}
-            game = chess.pgn.read_game(pgn)
-            if game is None:
-                break
-            row['headers'] = game.headers.__dict__
-            row['moves'] = [x.uci() for x in game.mainline_moves()]
-            rows.append(row)
-    games = pd.DataFrame(rows)
-    return games
+            # Convert createdAt to Unix time in milliseconds if it exists
+            game_date_unix = int(game['createdAt']) if 'createdAt' in game else None
 
-def process_games_data(games, player_name):
-    games['lichess_id'] = games['headers'].apply(lambda x: x.get("_tag_roster", {}).get("Site", ""))
-    games['white_elo'] = games['headers'].apply(lambda x: safe_convert_to_int(x.get("_others", {}).get("WhiteElo", "").split("-")[0]))
-    games['black_elo'] = games['headers'].apply(lambda x: safe_convert_to_int(x.get("_others", {}).get("BlackElo", "").split("-")[0]))
-    games['ECO'] = games['headers'].apply(lambda x: x.get("_others", {}).get("ECO"))
-    games['opening_name'] = games['headers'].apply(lambda x: x.get("_others", {}).get("Opening", ""))
-    games['event'] = games['headers'].apply(lambda x: x.get("_tag_roster", {}).get("Event", ""))
-    games['player_color'] = games['headers'].apply(lambda x: find_player_color(x, player_name))
-    games['score'] = games.apply(lambda x: player_score(x['headers'], x['player_color']), axis=1)
-    games['opening_name'] = games['opening_name'].apply(ftfy.fix_encoding)
-    games = games[games['ECO'] != '?']
-    return games
+            game_meta = {
+                'id': game.get('id'),
+                'white_elo': game['players']['white'].get('rating', None),
+                'black_elo': game['players']['black'].get('rating', None),
+                'white_player': game['players']['white']['user'].get('name', None),
+                'black_player': game['players']['black']['user'].get('name', None),
+                'winner': game.get('winner'),
+                'win_status': win_status,
+                'ECO': game.get('opening', {}).get('eco', 'Unknown'),
+                'opening_name': game.get('opening', {}).get('name', 'Unknown'),
+                'game_type': f"{game.get('variant')} - {game.get('speed')} - {game.get('perf')}",
+                'moves': moves_up_to_midgame_plus_8,
+                'division': game.get('division', {}),
+                'game_date_unix': game_date_unix  # Unix timestamp of the game creation
+            }
+            games_shorter.append(game_meta)
 
-def safe_convert_to_int(value):
-    try:
-        return int(value)
-    except ValueError:
-        return None  # or you can use np.nan or a placeholder like -1
-
-def find_player_color(header, player_name):
-    if header.get("_tag_roster", {}).get("White", "") == player_name:
-        return 'White'
-    elif header.get("_tag_roster", {}).get("Black", "") == player_name:
-        return 'Black'
-    return 'Unknown'
-
-def player_score(header, player_color):
-    result = header.get('_tag_roster', {}).get('Result', '')
-    if result == "1/2-1/2":
-        return 0.5  # Draw
-    elif result == "1-0" and player_color == 'White':
-        return 1.0  # White win
-    elif result == "0-1" and player_color == 'Black':
-        return 1.0  # Black win
-    return 0.0  # Loss or unknown result
-
-def save_to_csv(games, file_name):
-    games.to_csv(f"../../testData/{file_name}.csv", index=False)
-
-def load_from_csv(file_name):
-    return pd.read_csv(f"../../testData/{file_name}.csv")
-
-
-
-
-
-if __name__ == "__main__":
-    # Define the parameters
-    file_name = "apendra_games"
-    player_name = "Apendra"
-    NUM_GAMES = 1800  # From https://database.nikonoel.fr/
-
-    # Step 1: Read PGN files and create a DataFrame
-    games = read_pgn_create_dataframe(file_name, NUM_GAMES, player_name)
-
-    # Step 2: Process and clean the data
-    games_processed = process_games_data(games, player_name)
-
-    # Step 3: Save the processed data to CSV
-    save_to_csv(games_processed, file_name)
-
-    # Optional: Load the data back from CSV (demonstrating the load function)
-    games_loaded = load_from_csv(file_name)
-
-    # Now, games_loaded contains the processed game data ready for further analysis or use
-    print(games_loaded.head())
-
+    return games_shorter
